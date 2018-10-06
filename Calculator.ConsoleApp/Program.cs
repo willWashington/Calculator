be4e97dcd108affd565fb6bb96bb6bf45ff609b4
w2 <william.washington@live.com>
2018-10-05 23:15:27 -0400

    Add Adder.

--- Calculator.ConsoleApp/Program.cs
+++ Calculator.ConsoleApp/Program.cs
@@ -7,9 +7,21 @@ using System.Threading.Tasks;
 namespace Calculator.ConsoleApp
 {
     class Program
-    {
+    {     
         static void Main(string[] args)
         {
+            var adder = new Adder();
+            var answer = adder.Add(2, 2);
+            Console.WriteLine(answer);
+            Console.ReadKey(); //vs hack for pause
+        }
+    }
+
+    public class Adder
+    {
+        public int Add(int operand1, int operand2)
+        {
+            return operand1 + operand2;
         }
     }
 }
